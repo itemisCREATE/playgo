a2830750b06ecd2ae6f10934eebe141167ef8652
Smadar <Smadar@132.76.83.237>
2015-06-11 16:27:10 +0300

    Introducing ExecutionBridge

--- org.yakindu.sct.generator.playgo/src/org/yakindu/sct/generator/playgo/PlaygoStatemachine.xtend
+++ org.yakindu.sct.generator.playgo/src/org/yakindu/sct/generator/playgo/PlaygoStatemachine.xtend
@@ -15,7 +15,7 @@ class PlaygoStatemachine extends Statemachine {
 		package «flow.getImplementationPackageName(entry)»;
 		«flow.createImports(entry)»
 		
-		public class «flow.statemachineClassName» extends PlayableFramework implements «flow.statemachineInterfaceName» {
+		public class «flow.statemachineClassName» extends ExecutionBridge implements «flow.statemachineInterfaceName» {
 			«flow.createFieldDeclarations(entry)»
 		
 				
@@ -53,7 +53,7 @@ class PlaygoStatemachine extends Statemachine {
 	'''
 
 	override protected createImports(ExecutionFlow flow, GeneratorEntry entry) {
-		return super.createImports(flow, entry) + '''import il.ac.wis.cs.playgo.playtoolkit.container.PlayableFramework;'''
+		return super.createImports(flow, entry) + '''import il.ac.wis.cs.playgo.playtoolkit.ebridge.ExecutionBridge;'''
 	}
 	
 	def protected initialize(ExecutionFlow flow) '''
@@ -71,22 +71,28 @@ class PlaygoStatemachine extends Statemachine {
 		}
 	'''
 	
-	override generateEventDefinition(EventDefinition event, GeneratorEntry entry, InterfaceScope scope){ '''
-		private boolean «event.symbol»;
-		
+	override protected def generateInEventDefinition(EventDefinition event){
+		val className = event.scope.interfaceName.substring(3);
+		'''
 		«IF event.type != null && !isSame(event.type, getType(GenericTypeSystem.VOID))»
-			private «event.type.targetLanguageName» «event.valueIdentifier»;
+			public void raise«event.name.asName»(«event.type.targetLanguageName» value) {
+				«event.symbol» = true;
+				«event.valueIdentifier» = value;
+				systemEvent("«className»", "«event.name»");
+			}
+			
+			private «event.type.targetLanguageName» get«event.name.asName»Value() {
+				«event.getIllegalAccessValidation()»
+				return «event.valueIdentifier»;
+			}
+			
+		«ELSE»
+			public void raise«event.name.asName»() {
+				«event.symbol» = true;
+				systemEvent("«className»", "«event.name»");
+			}
+			
 		«ENDIF»
-		
-		«IF event.direction == Direction::IN»
-			«event.generateInEventDefinition»
-		«ENDIF»
-		
-		«IF event.direction == Direction::OUT»
-			«event.generateOutEventDefinition(entry, scope)»
-		«ENDIF»
-		Utils.invokeMethod(new String("«scope.interfaceName»").substring(3), "«event.name»", null, null);
 	'''
-
 	}
 }
--- org.yakindu.sct.generator.playgo/xtend-gen/org/yakindu/sct/generator/playgo/PlaygoStatemachine.java
+++ org.yakindu.sct.generator.playgo/xtend-gen/org/yakindu/sct/generator/playgo/PlaygoStatemachine.java
@@ -2,7 +2,6 @@ package org.yakindu.sct.generator.playgo;
 
 import com.google.common.base.Objects;
 import org.eclipse.xtend2.lib.StringConcatenation;
-import org.yakindu.base.types.Direction;
 import org.yakindu.base.types.Type;
 import org.yakindu.base.types.typesystem.GenericTypeSystem;
 import org.yakindu.sct.generator.java.Statemachine;
@@ -31,7 +30,7 @@ public class PlaygoStatemachine extends Statemachine {
     _builder.append("public class ");
     String _statemachineClassName = this._naming.statemachineClassName(flow);
     _builder.append(_statemachineClassName, "");
-    _builder.append(" extends PlayableFramework implements ");
+    _builder.append(" extends ExecutionBridge implements ");
     String _statemachineInterfaceName = this._naming.statemachineInterfaceName(flow);
     _builder.append(_statemachineInterfaceName, "");
     _builder.append(" {");
@@ -142,7 +141,7 @@ public class PlaygoStatemachine extends Statemachine {
   protected CharSequence createImports(final ExecutionFlow flow, final GeneratorEntry entry) {
     CharSequence _createImports = super.createImports(flow, entry);
     StringConcatenation _builder = new StringConcatenation();
-    _builder.append("import il.ac.wis.cs.playgo.playtoolkit.container.PlayableFramework;");
+    _builder.append("import il.ac.wis.cs.playgo.playtoolkit.ebridge.ExecutionBridge;");
     return (_createImports + _builder.toString());
   }
   
@@ -178,67 +177,108 @@ public class PlaygoStatemachine extends Statemachine {
   }
   
   @Override
-  public CharSequence generateEventDefinition(final EventDefinition event, final GeneratorEntry entry, final InterfaceScope scope) {
-    StringConcatenation _builder = new StringConcatenation();
-    _builder.append("private boolean ");
-    String _symbol = this._naming.getSymbol(event);
-    _builder.append(_symbol, "");
-    _builder.append(";");
-    _builder.newLineIfNotEmpty();
-    _builder.newLine();
+  protected CharSequence generateInEventDefinition(final EventDefinition event) {
+    CharSequence _xblockexpression = null;
     {
-      boolean _and = false;
-      Type _type = event.getType();
-      boolean _notEquals = (!Objects.equal(_type, null));
-      if (!_notEquals) {
-        _and = false;
-      } else {
-        Type _type_1 = event.getType();
-        Type _type_2 = this._iTypeSystem.getType(GenericTypeSystem.VOID);
-        boolean _isSame = this._iTypeSystem.isSame(_type_1, _type_2);
-        boolean _not = (!_isSame);
-        _and = _not;
+      InterfaceScope _scope = this._navigation.scope(event);
+      String _interfaceName = this._naming.getInterfaceName(_scope);
+      final String className = _interfaceName.substring(3);
+      StringConcatenation _builder = new StringConcatenation();
+      {
+        boolean _and = false;
+        Type _type = event.getType();
+        boolean _notEquals = (!Objects.equal(_type, null));
+        if (!_notEquals) {
+          _and = false;
+        } else {
+          Type _type_1 = event.getType();
+          Type _type_2 = this._iTypeSystem.getType(GenericTypeSystem.VOID);
+          boolean _isSame = this._iTypeSystem.isSame(_type_1, _type_2);
+          boolean _not = (!_isSame);
+          _and = _not;
+        }
+        if (_and) {
+          _builder.append("public void raise");
+          String _name = event.getName();
+          String _asName = this._naming.asName(_name);
+          _builder.append(_asName, "");
+          _builder.append("(");
+          Type _type_3 = event.getType();
+          String _targetLanguageName = this._iCodegenTypeSystemAccess.getTargetLanguageName(_type_3);
+          _builder.append(_targetLanguageName, "");
+          _builder.append(" value) {");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          String _symbol = this._naming.getSymbol(event);
+          _builder.append(_symbol, "\t");
+          _builder.append(" = true;");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          String _valueIdentifier = this._naming.getValueIdentifier(event);
+          _builder.append(_valueIdentifier, "\t");
+          _builder.append(" = value;");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          _builder.append("systemEvent(\"");
+          _builder.append(className, "\t");
+          _builder.append("\", \"");
+          String _name_1 = event.getName();
+          _builder.append(_name_1, "\t");
+          _builder.append("\");");
+          _builder.newLineIfNotEmpty();
+          _builder.append("}");
+          _builder.newLine();
+          _builder.newLine();
+          _builder.append("private ");
+          Type _type_4 = event.getType();
+          String _targetLanguageName_1 = this._iCodegenTypeSystemAccess.getTargetLanguageName(_type_4);
+          _builder.append(_targetLanguageName_1, "");
+          _builder.append(" get");
+          String _name_2 = event.getName();
+          String _asName_1 = this._naming.asName(_name_2);
+          _builder.append(_asName_1, "");
+          _builder.append("Value() {");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          CharSequence _illegalAccessValidation = this.getIllegalAccessValidation(event);
+          _builder.append(_illegalAccessValidation, "\t");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          _builder.append("return ");
+          String _valueIdentifier_1 = this._naming.getValueIdentifier(event);
+          _builder.append(_valueIdentifier_1, "\t");
+          _builder.append(";");
+          _builder.newLineIfNotEmpty();
+          _builder.append("}");
+          _builder.newLine();
+          _builder.newLine();
+        } else {
+          _builder.append("public void raise");
+          String _name_3 = event.getName();
+          String _asName_2 = this._naming.asName(_name_3);
+          _builder.append(_asName_2, "");
+          _builder.append("() {");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          String _symbol_1 = this._naming.getSymbol(event);
+          _builder.append(_symbol_1, "\t");
+          _builder.append(" = true;");
+          _builder.newLineIfNotEmpty();
+          _builder.append("\t");
+          _builder.append("systemEvent(\"");
+          _builder.append(className, "\t");
+          _builder.append("\", \"");
+          String _name_4 = event.getName();
+          _builder.append(_name_4, "\t");
+          _builder.append("\");");
+          _builder.newLineIfNotEmpty();
+          _builder.append("}");
+          _builder.newLine();
+          _builder.newLine();
+        }
       }
-      if (_and) {
-        _builder.append("private ");
-        Type _type_3 = event.getType();
-        String _targetLanguageName = this._iCodegenTypeSystemAccess.getTargetLanguageName(_type_3);
-        _builder.append(_targetLanguageName, "");
-        _builder.append(" ");
-        String _valueIdentifier = this._naming.getValueIdentifier(event);
-        _builder.append(_valueIdentifier, "");
-        _builder.append(";");
-        _builder.newLineIfNotEmpty();
-      }
+      _xblockexpression = _builder;
     }
-    _builder.newLine();
-    {
-      Direction _direction = event.getDirection();
-      boolean _equals = Objects.equal(_direction, Direction.IN);
-      if (_equals) {
-        CharSequence _generateInEventDefinition = this.generateInEventDefinition(event);
-        _builder.append(_generateInEventDefinition, "");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    _builder.newLine();
-    {
-      Direction _direction_1 = event.getDirection();
-      boolean _equals_1 = Objects.equal(_direction_1, Direction.OUT);
-      if (_equals_1) {
-        CharSequence _generateOutEventDefinition = this.generateOutEventDefinition(event, entry, scope);
-        _builder.append(_generateOutEventDefinition, "");
-        _builder.newLineIfNotEmpty();
-      }
-    }
-    _builder.append("Utils.invokeMethod(new String(\"");
-    String _interfaceName = this._naming.getInterfaceName(scope);
-    _builder.append(_interfaceName, "");
-    _builder.append("\").substring(3), \"");
-    String _name = event.getName();
-    _builder.append(_name, "");
-    _builder.append("\", null, null);");
-    _builder.newLineIfNotEmpty();
-    return _builder;
+    return _xblockexpression;
   }
 }
